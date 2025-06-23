a8b5b5d2e4d1f42e145531fcbf450101b20d9100
unknown <email2ashfak@gmail.com>
2025-06-23 22:36:36 +0600

    Login feature added

---
+++ tests/login.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test');
+
+test('Valid login works', async ({ page }) => {
+//কোড লিখুন
+});
+
+test('Invalid login shows error', async ({ page }) => {
+//কোড লিখুন
+});
